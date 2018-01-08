8f84441ccaa7ec718ef604975a6a59df918805bf
Unknown <h5061617@gmail.com>
2018-01-08 16:11:29 +0800

    2018 0108 gitkraken test
    test

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+
+
+    int i=0;
+    for(i=0;i<4;i++){
+        printf("\n");
+        printf("%d",i);
+    }
+    return 0;
+}
